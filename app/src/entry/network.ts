617d59bd13fb44a70f8d3cff7950dc58e8f07a3c
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-05-06 17:44:05 -0300

    network url

--- app/src/entry/network.ts
+++ app/src/entry/network.ts
@@ -1,7 +1,9 @@
 import { Code } from "./apk"
 
+const ENTRY_NETWORK_URL = "https://dolar.openapps.ar/_app/v1/app-entry.json";
+
 export const get_code_from_network = async (): Promise<Code> => {
-  const res = await fetch("/_app/app-entry.json");
+  const res = await fetch(ENTRY_NETWORK_URL);
   if(!res.ok) throw new Error(`network code fetch status not ok: ${res.status} ${res.statusText}`)
   const entry = await res.json();
   return entry;

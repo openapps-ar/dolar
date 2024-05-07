90bd62d2c1953d08054dd0abe4804ed00d6b8fbf
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-05-07 17:01:22 -0300

    pm2

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     instances: 1,
     script: "./api/dist/node/server.js",
     // args: ["start"],
-    interpreter_args: [],
+    interpreter_args: [ "--no-warnings", "--experimental-fetch" ],
     name: "dolar",
     time: true,
     // env: { 

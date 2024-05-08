5b5df170d5b40050b04b386bcbbc527334cc7765
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-05-08 00:46:39 -0300

    pm2

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -4,8 +4,9 @@ module.exports = {
     instances: 1,
     script: "./api/dist/node/server.js",
     // args: ["start"],
-    interpreter_args: [ "--no-warnings", "--experimental-fetch" ],
+    interpreter_args: [],
     name: "dolar",
+    merge_logs: true,
     time: true,
     // env: { 
     //   FORCE_COLOR: "1",
